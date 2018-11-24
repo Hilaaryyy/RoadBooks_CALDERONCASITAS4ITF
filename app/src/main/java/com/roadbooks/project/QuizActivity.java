b987bd5fb07b5d4aa35b29bc2adb1831af7ff1b4
imjanellarazel <janellarazelcalderon14@gmail.com>
2018-11-25 00:38:25 +0800

    Quiz - Working user choices(identify right and wrong answer) up to 5 questions random

--- app/src/main/java/com/roadbooks/project/QuizActivity.java
+++ app/src/main/java/com/roadbooks/project/QuizActivity.java
@@ -118,15 +118,15 @@ public class QuizActivity extends AppCompatActivity {
         switch(currentQuestion.getAnswerNr()){
             case 1:
                 rb1.setTextColor(Color.GREEN);
-                textViewQuestion.setText("Answer 1 is correct");
+                textViewQuestion.setText("Answer 1 is correct!");
                 break;
             case 2:
                 rb2.setTextColor(Color.GREEN);
-                textViewQuestion.setText("Answer 2 is correct");
+                textViewQuestion.setText("Answer 2 is correct!");
                 break;
             case 3:
                 rb3.setTextColor(Color.GREEN);
-                textViewQuestion.setText("Answer 3 is correct");
+                textViewQuestion.setText("Answer 3 is correct!");
                 break;
         }
         if (questionCounter < questionCountTotal){
