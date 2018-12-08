757531446a2de31b8c80b1551113f6c1d90eefc2
Hilary Casitas <2015081522@ust-ics.mygbiz.com>
2018-12-08 16:55:08 +0800

    grades pt2

---
+++ app/src/main/java/com/roadbooks/project/gques.java
@@ -0,0 +1,54 @@
+package com.roadbooks.project;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class gques extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gques);
+        Intent i = new Intent(this, CustomService.class);
+        startService(i);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        Menu menu = bottomNav.getMenu();
+        MenuItem menuItem = menu.getItem(2);
+        menuItem.setChecked(true);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_assign:
+                        Intent intent1 = new Intent(gques.this, Review.class);
+                        gques.this.startActivity(intent1);
+                        break;
+                    case R.id.nav_quiz:
+                        Intent intent2 = new Intent(gques.this, Quiz.class);
+                        gques.this.startActivity(intent2);
+                        break;
+                    case R.id.nav_grades:
+                        break;
+
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+
+
+}
